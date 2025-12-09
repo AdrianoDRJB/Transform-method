2ec1523db027b59055129e441bfdafad59f5bcaf
Adriano <adriano.nutrition@gmail.com>
2025-12-08 20:10:31 -0500

    Revert improvements - back to stable version

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ import PlansPage from './components/PlansPage'
 import SuccessPage from './components/SuccessPage'
 import BlackFridayOffer from './components/BlackFridayOffer'
 import TheIdentity from './components/TheIdentity'
-import VideoPage from './components/VideoPage'
 import './App.css'
 
 function AppContent() {
@@ -19,15 +18,12 @@ function AppContent() {
   const location = useLocation()
   const isIdentityPage = location.pathname === '/identity' || location.pathname === '/transform2026'
 
-  // If identity, transform2026, or video page, render ONLY the component without any wrapper
+  // If identity or transform2026 page, render ONLY the component without any wrapper
   if (location.pathname === '/identity') {
     return <TheIdentity />
   }
   if (location.pathname === '/transform2026') {
-    return <TheIdentity />
-  }
-  if (location.pathname === '/video') {
-    return <VideoPage />
+    return <Transform2026 />
   }
 
   // Normal pages with navbar and footer
